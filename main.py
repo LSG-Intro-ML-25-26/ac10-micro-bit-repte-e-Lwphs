5c4871c827d69b310165a28aecd1670e1de92851
Arnau Garcia <arnau.garcia@gracia.lasalle.cat>
2025-12-10 20:55:23 +0100

    Feat: Repte-e acabat

--- main.py
+++ main.py
@@ -1,1 +1,44 @@
+gota = False
+temperatura = False
+x = 2
+y = 2
 
+def dibuixarGrafic():
+    led.plot_bar_graph(input.temperature(), 50)
+
+def dibuixarGota():
+    global x
+    global y
+    accX = input.acceleration(Dimension.X)
+    accY = input.acceleration(Dimension.Y)
+    led.plot(x, y)
+    basic.pause(50)
+    basic.clear_screen()
+    if accX <= 150 and x > 0:
+        x = x - 1
+    if accX > 150 and x < 4:
+        x = x + 1
+    if accY <= 150 and y > 0:
+        y = y - 1
+    if accY > 150 and y < 4:
+        y = y + 1
+
+    led.plot(x, y)
+
+def bucle():
+    global temperatura 
+    global gota
+    if input.button_is_pressed(Button.A):
+        temperatura = True
+        gota = False
+    elif input.button_is_pressed(Button.B):
+        temperatura = False
+        gota = True
+    
+    if temperatura:
+        dibuixarGrafic()
+    elif gota:
+        dibuixarGota()
+
+while True:
+    bucle()
--- main.ts
+++ main.ts
@@ -1,1 +1,57 @@
+let gota = false
+let temperatura = false
+let x = 2
+let y = 2
+function dibuixarGrafic() {
+    led.plotBarGraph(input.temperature(), 50)
+}
 
+function dibuixarGota() {
+    
+    
+    let accX = input.acceleration(Dimension.X)
+    let accY = input.acceleration(Dimension.Y)
+    led.plot(x, y)
+    basic.pause(50)
+    basic.clearScreen()
+    if (accX <= 150 && x > 0) {
+        x = x - 1
+    }
+    
+    if (accX > 150 && x < 4) {
+        x = x + 1
+    }
+    
+    if (accY <= 150 && y > 0) {
+        y = y - 1
+    }
+    
+    if (accY > 150 && y < 4) {
+        y = y + 1
+    }
+    
+    led.plot(x, y)
+}
+
+function bucle() {
+    
+    
+    if (input.buttonIsPressed(Button.A)) {
+        temperatura = true
+        gota = false
+    } else if (input.buttonIsPressed(Button.B)) {
+        temperatura = false
+        gota = true
+    }
+    
+    if (temperatura) {
+        dibuixarGrafic()
+    } else if (gota) {
+        dibuixarGota()
+    }
+    
+}
+
+while (true) {
+    bucle()
+}
